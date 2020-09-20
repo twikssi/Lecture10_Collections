d318fd33d5f7278d1f52dc6acd783c5cc481e823
twikssi <limpbiski15@gmail.com>
2020-09-20 20:57:38 +0300

    created method addProduct, two constructors and getter in shop class

--- src/main/java/by/andrey/twikssi/exer3library/service/Shop.java
+++ src/main/java/by/andrey/twikssi/exer3library/service/Shop.java
@@ -1,5 +1,26 @@
 package by.andrey.twikssi.exer3library.service;
 
-public class Shop {
+import by.andrey.twikssi.exer3library.bean.Product;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+    private List<Product> shopProducts;
+
+    public Shop() {
+        shopProducts = new ArrayList<>();
+    }
+
+    public Shop(List<Product> products) {
+        this.shopProducts = products;
+    }
+
+    public List<Product> getShopProducts() {
+        return shopProducts;
+    }
+
+    public void addProduct(Product product){
+        shopProducts.add(product);
+    }
 }
