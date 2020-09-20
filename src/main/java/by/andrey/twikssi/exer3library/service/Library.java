f635b24b1a29e78fd70c0bea0707759338257cbb
twikssi <limpbiski15@gmail.com>
2020-09-20 19:10:24 +0300

    created class library and method addBook and deleteBook in this class

---
+++ src/main/java/by/andrey/twikssi/exer3library/service/Library.java
@@ -0,0 +1,22 @@
+package by.andrey.twikssi.exer3library.service;
+
+import by.andrey.twikssi.exer3library.bean.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> library;
+
+    public Library() {
+        this.library =new ArrayList<>();
+    }
+
+    public void addBook(Book book){
+        library.add(book);
+    }
+
+    public void deleteBook(Book book){
+        library.remove(book);
+    }
+}
